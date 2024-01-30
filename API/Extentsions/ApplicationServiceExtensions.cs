6779543d0a23e1223231159030a3a7a83046c2a2
Ümit Sengül <umitsengul.de@gmail.com>
2024-01-30 23:26:33 +0300

    8 bölüm sonu

--- API/Extentsions/ApplicationServiceExtensions.cs
+++ API/Extentsions/ApplicationServiceExtensions.cs
@@ -1,20 +1,27 @@
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
+using AutoMapper;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace API.Extensions;
-
-public static class ApplicationServiceExtensions
+namespace API.Extensions
 {
-    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    public static class ApplicationServiceExtensions
     {
-        services.AddScoped<ITokenService, TokenService>();
-        services.AddDbContext<DataContext>(opt =>
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
-            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-        });
+            services.AddScoped<ITokenService, TokenService>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+            });
 
-        return services;
+            return services;
+        }
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -1,7 +1,11 @@
 using System.Text;
+using API.Data;
+using API.Entities;
 using API.Extensions;
 using API.Middleware;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,4 +28,21 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
 app.Run();
+
+
+
